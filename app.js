06ca893ab90e8e75d35b19575539b7d41e54643f
Jayant Arora <jaistudy1996@gmail.com>
2016-10-16 19:48:21 -0400

    Add login controller to main app

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var db = require('mysql');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var profile = require('./routes/profile');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -48,6 +49,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/profile', profile);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
