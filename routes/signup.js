7e08967fb032f1e0e42702c7f281c25cba909cf1
Jayant Arora <jaistudy1996@gmail.com>
2016-10-21 19:55:55 -0400

    Setup signup controller

---
+++ routes/signup.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.router();
+var db = require('mysql');
+
+// POST sign up controller
+
+router.post('/', function(req, res, next){
+	
+});
+
+
+
+module.exports = router; 
