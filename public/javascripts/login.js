aee4284af0ca0d50c6e6e8246b5330fdcaeaf963
Jayant Arora <jaistudy1996@gmail.com>
2016-10-21 12:34:49 -0400

    Login client controller debug

--- public/javascripts/login.js
+++ public/javascripts/login.js
@@ -2,5 +2,20 @@
 // Login JS for client side webpage. 
 
 function login(){
-	
+	console.log("hello");
+	var username = document.getElementById("uname").value;
+	var password = document.getElementById("psw").value;
+	var params = "uname="+username+"&psw="+password;
+	var url = "/";
+	var http = new XMLHttpRequest();
+	console.log("login func start");
+	http.open("POST", url, true);
+	http.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	http.onreadystatechange() = function() {
+		if(this.readyState == 4 && this.status == 200){
+			document.getElementById("loginButton").value = "Login Success";
+		}
+	}
+	http.send(params);	
 }
+
