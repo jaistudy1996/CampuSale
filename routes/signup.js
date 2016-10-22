e092271e5d916c7af641314af4bbf3267d5424ef
Jayant Arora <jaistudy1996@gmail.com>
2016-10-21 20:19:24 -0400

    Reply with a sample reply to client trying to signup

--- routes/signup.js
+++ routes/signup.js
@@ -1,11 +1,11 @@
 var express = require('express');
-var router = express.router();
+var router = express.Router();
 var db = require('mysql');
 
 // POST sign up controller
 
 router.post('/', function(req, res, next){
-	
+	res.send("Reply from sign up controller");
 });
 
 
