85b1dd627016e3476aa3c08ca1f27ca00f2dff2a
Jayant Arora <jaistudy1996@gmail.com>
2016-10-21 20:18:48 -0400

    Successful connection to server and parameter validation

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -9,9 +9,19 @@ function signup() {
 	var password = document.getElementById("signup4").value;
 	var confirm_pass = document.getElementById("signup5").value;
 	var url = "/signup";
-	if(typeof(email.slice(0, 5)) == "string" && typeof(email.slice(6) == "number")){
+	var http = new XMLHttpRequest();
+	var params = "fname="+fname+"&lname="+lname+"&email="+email+"&password="+password;
+	if(typeof(email.slice(0, 5)) == "string" && typeof(Number(email.slice(6))) == "number"){
 		if(password === confirm_pass){
 			var params = "fname="+fname+"&lname="+lname+"&email="+email+"&password="+password;
+			http.open("POST", url, true);
+			http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+			http.onreadystatechange = function() { 
+				if(this.readyState == 4 && this.status == 200){
+					console.log(http.responseText);
+				}
+			}
+			http.send(params);
 		}
 		else{
 			// show error for password not valid or do not match.
