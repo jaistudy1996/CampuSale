956c3c8e9c9f46c7a025f5dc1d2d0b363034b093
Jayant Arora <jaistudy1996@gmail.com>
2016-10-21 22:41:57 -0400

    Set up variables for given params

--- routes/signup.js
+++ routes/signup.js
@@ -2,9 +2,16 @@ var express = require('express');
 var router = express.Router();
 var db = require('mysql');
 
+// DB connection
+
 // POST sign up controller
 
 router.post('/', function(req, res, next){
+	var fname = req.body.fname;
+	var lname = req.body.lname;
+	var email = req.body.email;
+	var pass1 = req.body.password;
+	var pass2 = req.body.confirm_pass;
 	res.send("Reply from sign up controller");
 });
 
