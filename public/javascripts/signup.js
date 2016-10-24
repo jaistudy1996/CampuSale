f43b19ae3db4015a2c51455ccc5fb1d77c4b3f5f
Jayant Arora <jaistudy1996@gmail.com>
2016-10-24 09:10:39 -0400

    Code update to connect with server

--- public/javascripts/signup.js
+++ public/javascripts/signup.js
@@ -4,23 +4,23 @@
 function signup() {
 	var fname = document.getElementById("signup1").value;
 	var lname = document.getElementById("signup2").value;
-	var email = document.getElementById("signup3").value;
-	var phone_number = document.getElementById("").value;
-	var zip_code = document.getElementById("").value;
+	var email = document.getElementById("signup5").value;
+	var phone_number = document.getElementById("signup3").value;
+	var zip_code = document.getElementById("signup4").value;
 	// This is temporary. Will be removed after implementation of CAS protocol.
-	var password = document.getElementById("signup4").value;
-	var confirm_pass = document.getElementById("signup5").value;
+	var password = document.getElementById("signup6").value;
+	var confirm_pass = document.getElementById("signup7").value;
 	var url = "/signup";
 	var http = new XMLHttpRequest();
-	var params = "fname="+fname+"&lname="+lname+"&email="+email+"&password="+password;
+	var params = "fname="+fname+"&lname="+lname+"&email="+email+"&password="+password+"&confirm_pass="+confirm_pass+"&phone_number="+phone_number+"&zip="+zip_code;
 	if(typeof(email.slice(0, 5)) == "string" && typeof(Number(email.slice(6))) == "number"){
 		if(password === confirm_pass){
-			var params = "fname="+fname+"&lname="+lname+"&email="+email+"&password="+password+"&confirm_pass="+confirm_pass;
 			http.open("POST", url, true);
 			http.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 			http.onreadystatechange = function() { 
 				if(this.readyState == 4 && this.status == 200){
 					console.log(http.responseText);
+					console.log(params);
 				}
 			}
 			http.send(params);
