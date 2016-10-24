38cd862bc30b059bd913d9348febc982502a77ea
Jayant Arora <jaistudy1996@gmail.com>
2016-10-24 09:11:05 -0400

    User sign up complete. Pass not encrypted

--- routes/signup.js
+++ routes/signup.js
@@ -3,6 +3,12 @@ var router = express.Router();
 var db = require('mysql');
 
 // DB connection
+dbconn = db.createConnection({
+	host: '137.142.1.54',
+	user: "amos",
+	password: "olasoji",
+	database: "campuSale",
+});
 
 // POST sign up controller
 
@@ -10,9 +16,21 @@ router.post('/', function(req, res, next){
 	var fname = req.body.fname;
 	var lname = req.body.lname;
 	var email = req.body.email;
+	var phone_num = req.body.phone_number;
+	var zip = req.body.zip;
 	var pass1 = req.body.password;
 	var pass2 = req.body.confirm_pass;
-	res.send("Reply from sign up controller");
+
+	// SQL query
+	dbconn.query({sql: "INSERT INTO students SET ?", values: {firstName: fname, lastName: lname, email: email, phone: phone_num, zip: zip, password: pass1}}, function(err, result){
+		if(err){
+			console.log(err);
+			res.send(err);
+		}
+	});
+	
+	console.log( fname, lname, email, phone_num, zip, pass1, pass2);
+	res.send("Reply from sign up controller: ", fname, lname, email, phone_num, zip, pass1, pass2);
 });
 
 
