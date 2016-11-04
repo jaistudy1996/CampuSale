10d7438b27769868e6c9c7d8e5a690a6431ebe0f
aolas001 <amos.olasoji@gmail.com>
2016-11-04 19:22:07 -0400

    Update selling.js
    Making changes to the Selling route

--- routes/selling.js
+++ routes/selling.js
@@ -1,12 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var db = require('mysql');
 
-// DB connection
-dbconn = db.createConnection({
-	host: '137.142.1.54',
-	user: "amos",
-	password: "olasoji",
-	database: "campuSale",
-	//debug: true,
-});::
+router.get("/", function(req, res, next){
+	res.render("selling", {});
+});
+
+module.exports = router;
