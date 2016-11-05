409f2355784f20e39d1aa42d12e0c36563cc8b35
Jayant Arora <jaistudy1996@gmail.com>
2016-11-04 20:50:59 -0400

    Login validation complete

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var db = require('mysql');
 
 // Connect to database.
-var dbpool = db.createConnection({
+var dbconn = db.createConnection({
         //connectionLimit: 10,
         host: '137.142.1.54',
         user: 'amos',
@@ -17,7 +17,17 @@ var dbpool = db.createConnection({
 router.post('/', function(req, res, next) {
 	console.log(req.body.uname);
 	console.log(req.body.psw);
-
+	var uname = req.body.uname;
+	var psw = req.body.psw;
+	//DB query
+	dbconn.query("SELECT * FROM students where email = ? and password = password(?) LIMIT 1", [uname, psw], function(err, result){
+		if(err){
+			console.log(err);
+			res.send(err);
+		}
+		console.log(result);
+	
+	});
 	res.send("Response from server");
 });
 
