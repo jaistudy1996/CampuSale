58738a91a90dd6a7dced98a940573fdc1dbdbd34
Jayant Arora <jaistudy1996@gmail.com>
2016-11-07 09:41:29 -0500

    Add db connection to get tag names from the database

--- routes/listing.js
+++ routes/listing.js
@@ -1,7 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var dbconn = require('mysql');
+
+var db = dbconn.createConnection({
+		host: "137.142.1.54",
+		user: "amos",
+		password: "olasoji",
+		database: "campuSale"
+	}); 
 
 router.get('/', function(req, res, next){
+	db.query("SELECT * FROM tags", function(err, result){
+		if(err){
+			res.send(err);
+		}
+		console.log(result);
+	});
 	res.render("listing", {});
 });
 
