e1974f60d9796abbd90d84e93425a8b661e0fc53
Jayant Arora <jaistudy1996@gmail.com>
2016-11-09 12:39:32 -0500

    Add a sperate function to edit HTML

--- public/javascripts/listing.js
+++ public/javascripts/listing.js
@@ -2,10 +2,47 @@
 // Description: Function for listing page.
 
 function category(){
+	/*
 	var select = document.getElementById("Category");
-	var link = "Link1";
-	var link2 = "Link2";
-	document.write(link.link("#"));
-	document.write(link2.link("#"));
-	console.log("Category Function");
+	var links = [{link1: "link1"}, {link1: "link2"}, {link1: "link3"}];
+	
+	for(var i=0; i<links.length; i++){
+		var link2 = links[i];
+		console.log(link2);
+		document.write(link2.link1.link("#"));
+	}
+	*/
+	var sendObj = []
+	var url = "/listing/tags";
+	var xhr = new XMLHttpRequest();
+	xhr.responseType = 'json';
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.onreadystatechange = function(){
+		if(this.readyState == 4 && this.status == 200){
+			//var select = document.getElementById("Category");
+			for(var i=0; i<xhr.response.length; i++){
+				//var option = xhr.response[i];
+				//var name = option.tagName;
+				//document.write(name.link("#"));
+				//console.log(xhr.response[i]);
+				//console.log(option.tagName, option.tagID.toString());
+			}
+			sendObj = xhr.response;
+			console.log(xhr.response.length);
+			addLinks(sendObj);
+		}	
+	}
+	xhr.send(null);
+
+}
+
+function addLinks(object){
+	var select = document.getElementById("Category");
+	console.log(object);
+	
+	for(var i = 0; i < object.length; i++){
+		console.log(object[i]);
+		document.getElementById("Category").innerHTML += object[i].tagName.link(object[i].tagID);
+	}
 }
