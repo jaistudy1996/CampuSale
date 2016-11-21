05ecc9799be4e84db2212cc639773f897863d935
Jayant Arora <jaistudy1996@gmail.com>
2016-11-21 13:19:13 -0500

    Add a new post route for item image upload

--- routes/item.js
+++ routes/item.js
@@ -9,4 +9,8 @@ router.get("/:itemID", function(req, res, next){
 	res.redirect("/item");
 });
 
+router.post("/upload", function(req, res, next){
+	console.log("Item upload");
+});
+
 module.exports = router;
