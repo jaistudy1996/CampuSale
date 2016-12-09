08cece5df3f41031c0a92531abeb4eb2728c5917
Jayant Arora <jaistudy1996@gmail.com>
2016-12-09 12:19:07 -0500

    Update data on profile page dynamically

--- routes/profile.js
+++ routes/profile.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
+var db = require('mysql');
+
+// Database login
+var dbconn = db.createConnection({
+        //connectionLimit: 10,
+        host: '137.142.1.54',
+        user: 'amos',
+        password: 'olasoji',
+        database: 'campuSale'
+});
 
 
 // GET request
 router.get('/', function(req, res, next){
 	user = req.signedCookies.loginName;
 	if(user){
-		res.render('profile', {Username: user});
+		res.render('profile', {Name: user, Username: user});
 	}
 	else{
 		res.redirect('/');
